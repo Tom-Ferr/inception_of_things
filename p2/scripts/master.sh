c93ec8d8e711a2b5f12dfc80b3e1bede34217814
Tom-Ferr <ferr.wav@gmail.com>
2023-12-05 19:09:13 -0300

    first commit

---
+++ p2/scripts/master.sh
@@ -0,0 +1,9 @@
+apt update
+apt install -y curl wget systemd
+curl -sfL https://get.k3s.io |  sh -s - --flannel-iface=enp0s8
+systemctl status k3s
+mkdir /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml $KUBECONFIG && chown vagrant $KUBECONFIG
+chmod 600 $KUBECONFIG
+echo "export KUBECONFIG=$KUBECONFIG" >> /etc/profile.d/myvar.sh
+cp /var/lib/rancher/k3s/server/node-token /home/vagrant/.confs/node-token
