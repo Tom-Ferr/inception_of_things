026f7f77d049c965977a74334fae0eccfe47ed51
Tom-Ferr <ferr.wav@gmail.com>
2023-12-10 15:09:43 -0300

    rollout

--- p2/scripts/master.sh
+++ p2/scripts/master.sh
@@ -9,10 +9,12 @@ echo "192.168.56.110 app1.com" >> /etc/hosts
 echo "192.168.56.110 app2.com" >> /etc/hosts
 echo "192.168.56.110 app3.com" >> /etc/hosts
 kubectl apply -f .confs/nginx-controller.yaml
-echo waiting for nginx-controller to be ready
-kubectl wait --for=condition=Ready=True deployment.apps/ingress-nginx-controller -n ingress-nginx --timeout=120s
+kubectl rollout status deployment.apps/ingress-nginx-controller -n ingress-nginx
 kubectl apply -f .confs/load-balancer.yaml
 kubectl apply -f .confs/app-one.yaml
 kubectl apply -f .confs/app-two.yaml
 kubectl apply -f .confs/app-three.yaml
 kubectl apply -f .confs/ingress.yaml
+kubectl rollout status deployment.apps/app-one
+kubectl rollout status deployment.apps/app-two
+kubectl rollout status deployment.apps/app-three
